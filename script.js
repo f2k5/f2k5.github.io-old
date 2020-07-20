541741561d4222c74f2d5561e030c7041585525e
fardinkhan <fardinkhan@uvic.ca>
2020-07-19 17:21:31 -0700

    added a bunch of features

---
+++ script.js
@@ -0,0 +1,63 @@
+function displayGitHubText() {
+	document.querySelector("h6").innerHTML = "GitHub";
+	document.getElementById('x').style.color = 'black';
+}
+
+function hideGitHubText() {
+	document.querySelector("h6").innerHTML = "";
+}
+
+function displayTwitterText() {
+	document.querySelector("h6").innerHTML = "Twitter";
+	document.getElementById('x').style.color = 'black';
+}
+
+function hideTwitterText() {
+	document.querySelector("h6").innerHTML = "";
+}
+
+function displayInstagramText() {
+	document.querySelector("h6").innerHTML = "Instagram";
+	document.getElementById('x').style.color = 'black';
+}
+
+function hideInstagramText() {
+	document.querySelector("h6").innerHTML = "";
+}
+
+function displayEmailText() {
+	document.querySelector("h6").innerHTML = "Send me an email.";
+	document.getElementById('x').style.color = 'black';
+}
+
+function hideEmailText() {
+	document.querySelector("h6").innerHTML = "";
+}
+
+function displayLinkedInText() {
+	document.querySelector("h6").innerHTML = "LinkedIn";
+	document.getElementById('x').style.color = 'black';
+}
+
+function hideLinkedInText() {
+	document.querySelector("h6").innerHTML = "";
+}
+
+function displayResumeText() {
+	document.querySelector("h6").innerHTML = "Résumé";
+	document.getElementById('x').style.color = 'black';
+}
+
+function hideResumeText() {
+	document.querySelector("h6").innerHTML = "";
+}
+
+function displayNumber() {
+	document.querySelector("h6").innerHTML = "+1 778 551 2510";
+	document.getElementById('x').style.color = '#c93e48';
+	document.getElementById('x').style.fontSize = "25px";
+}
+
+function hideNumber() {
+	document.querySelector("h6").innerHTML = "";
+}
